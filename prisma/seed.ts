065422dd7670bd1172a1455bb3921d2a9d4412ab
Jamespino20 <espino.jamesbryant20@gmail.com>
2026-03-25 20:09:53 +0800

    PrismaClient export error minor fix

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,5 @@
-import { PrismaClient, Role, UserStatus } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+import { Role, UserStatus } from "@prisma/client";
 import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
